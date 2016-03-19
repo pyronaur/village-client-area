794528f9b46fa35202fc2cf3d65f8f69b3c223a9
Norris <im@justnorris.com>
2016-03-19 14:32:50 +0000

    Update: Make filter buttons translatable

--- templates/single/gallery-filters.php
+++ templates/single/gallery-filters.php
@@ -1,10 +1,8 @@
-
-
 <div class="ca-gallery-actions">
 
-	<div class="village-button ca-action js__ca-action is-active" data-action="all">Show All</div>
-	<div class="village-button ca-action js__ca-action" data-action="selected">Show Selected</div>
-	<div class="village-button ca-action js__ca-action" data-action="unselected">Show Unselected</div>
+	<div class="village-button ca-action js__ca-action is-active" data-action="all"><?php esc_html_e( 'Show All', 'village' ); ?></div>
+	<div class="village-button ca-action js__ca-action" data-action="selected"><?php esc_html_e( 'Show Selected', 'village' ); ?></div>
+	<div class="village-button ca-action js__ca-action" data-action="unselected"><?php esc_html_e( 'Show Unselected', 'village' ); ?></div>
 
 
 </div>
